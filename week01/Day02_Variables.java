1a844a273b9ffe1bbfc69bb7a22f438d4ece267f
LEEJUNHEE <b27jrjhwj6@privaterelay.appleid.com>
2025-10-28 10:00:41 +0900

    Day01_Variables

---
+++ week01/Day02_Variables.java
@@ -0,0 +1,13 @@
+public class Day02_Variables{
+    public static void main(String[] args){
+        int age = 20;
+        double weigth = 65.5;
+        char grade = 'A';
+        boolean isStudent = true;
+
+        System.out.println("Age: " + age);
+        System.out.println("Weigth: " + weigth);
+        System.out.println("Grade: " + grade);
+        System.out.println("isStudent: " + isStudent);
+    }
+}
